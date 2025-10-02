44621f51fd6485b5d942c9d85c71ac2da82eccbc
Hamzeh <h.qorbani7@gmail.com>
2025-10-02 15:07:43 +0330

    update messages

--- messages/errors.py
+++ messages/errors.py
@@ -1,3 +1,4 @@
 class ErrorMessages:
     ERROR_FILE_UPLOAD = "بارگزاری فایل با مشکل مواجه شد"
     ERROR_FILE_DELETE = "حذف فایل با خطا روبرو شد. مطمئن باشید فایلی را برای حذف انتخاب کرده باشید"
+    ERROR_PUBLISH_PDF = "خطا در ایجاد گزارش pdf"
--- messages/successes.py
+++ messages/successes.py
@@ -1,3 +1,4 @@
 class SuccessMessages:
     SUCCESS_FILE_UPLOAD = "بارگزاری فایل با موفقیت انجام شد"
     SUCCESS_FILE_DELETE = "حذف فایل با موفقیت انجام شد"
+    SUCCESS_PUBLISH_PDF = "'گزارش pdf با موفقیت ایجاد شد"
